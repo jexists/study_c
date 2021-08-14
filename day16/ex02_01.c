f06f989ad9f3a5ca8d3dc416b7a93271aeecef8b
jexists <54986331+jexists@users.noreply.github.com>
2021-08-14 22:56:59 +0900

    error check

---
+++ day16/ex02_01.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+struct Data{
+  char c1;
+  int *numberPointer;
+};
+
+int main(){
+  int num1 = 10;
+  // 구조체 타입의 변수
+  struct Data d;
+  // 구조체 타입의 포인터
+  struct Data *dPointer = malloc(sizeof(struct Data));
+
+  d.numberPointer = &num1;
+  dPointer -> numberPointer = &num1;
+
+  printf("*d.numberPointer : %d\n", *d.numberPointer);
+  printf("*dPointer -> numberPointer : %d\n", *dPointer -> numberPointer);
+
+  printf("d.numberPointer : %p\n", d.numberPointer);
+  printf("dPointer -> numberPointer : %p\n", dPointer -> numberPointer);
+
+  dPointer -> c1 = 'a';
+
+  printf("(*dPointer).c1 : %c\n", (*dPointer).c1);
+  printf("*(*dPointer).numberPointer : %d\n", *(*dPointer).numberPointer);
+
+  printf("(*dPointer).numberPointer : %p\n", (*dPointer).numberPointer);
+
+
+
+  free(dPointer);
+
+  return 0;
+}
+
